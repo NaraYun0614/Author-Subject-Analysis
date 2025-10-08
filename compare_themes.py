40227ab19f05025d790141910abae046b306eab6
NaraYun0614 <nara.yun@mail.mcgill.ca>
2025-10-08 12:35:29 -0400

    extract themes for 2 authors

---
+++ compare_themes.py
@@ -0,0 +1,14 @@
+import json
+import os.path
+
+script_path = os.path.dirname(__file__)
+raw_path = os.path.join(script_path)
+
+
+def main():
+
+
+    pass
+
+if __name__ == "__main__":
+    main()
--- extract_themes.py
+++ extract_themes.py
@@ -1,15 +1,35 @@
 import json
+import os.path
+
+script_path = os.path.dirname(__file__)
+raw_path = os.path.join(script_path)
+
 
 def main():
     # takes in argument of json file
 
-    with open('octavia_butler.json', 'r') as file: 
-        octavia_butler_data = json.load(file)
+    with open('john_green.json', 'r') as file: 
+        john_green_data = json.load(file)
     
-    octavia_subjects = []
+    john_subjects = []
+
+    for i in range(len(john_green_data['entries'])):
+        try:
+            john_subjects.extend(john_green_data['entries'][i]['subjects'])
+        except KeyError:
+            continue
+    print(set(john_subjects))
+
+    json_output = json.dumps(list(set(john_subjects)), indent=4)
+    print(json_output)
+
+    #octavia_data = set(octavia_subjects).json()
+    with open(os.path.join(raw_path, 'john_theme.json'), "w") as f:
+        json.dump(json_output, f, indent=4)
+       
     
-    for i in range(len(octavia_butler_data['entries'])):
-        print(octavia_butler_data['entries'][i]['subjects'])
+    # for i in range(len(octavia_butler_data['entries'])):
+    #     print(octavia_butler_data['entries'][i]['subjects'])
 
     # result = author_theme.json
 
